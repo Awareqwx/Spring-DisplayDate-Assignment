08e73bfb85025d297414b26d8df660a0e3c8e468
Awareqwx <benpeters97@yahoo.com>
2017-11-07 12:24:50 -0800

    Initial Commit

---
+++ src/main/java/com/awareqwx/DisplayDate/controllers/DateController.java
@@ -0,0 +1,31 @@
+package com.awareqwx.DisplayDate.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DateController
+{
+    
+    @RequestMapping("/")
+    public String index()
+    {
+        return "index.jsp";
+    }
+    
+    @RequestMapping("/date")
+    public String date(Model model)
+    {
+        model.addAttribute("date", new java.text.SimpleDateFormat("EEEE', the 'dd' of 'M', 'yyyy").format(new java.util.Date()));
+        return "date.jsp";
+    }
+    
+    @RequestMapping("/time")
+    public String time(Model model)
+    {
+        model.addAttribute("time", new java.text.SimpleDateFormat("hh:mm a").format(new java.util.Date()));
+        return "time.jsp";
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/date.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Date</title>
+</head>
+<body style="font-family: Arial">
+    <span id="date">${date}</span>
+</body>
+</html>
